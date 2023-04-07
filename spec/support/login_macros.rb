ef13c9ad10234432df1427471341836f001233c2
mugihouse <mugi.stnk100122@gmail.com>
2023-04-08 00:10:16 +0900

    Fix: password入力内容修正

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -3,7 +3,7 @@ module LoginMacros
     visit admin_login_identifier_path
     fill_in 'Username', with: user.name
     click_button '次へ'
-    fill_in 'Password', with: user.password
+    fill_in 'Password', with: 'password'
     click_button 'ログイン'
   end
 end
