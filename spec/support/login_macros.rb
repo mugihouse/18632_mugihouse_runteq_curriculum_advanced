6dfad2fd8c1a79241f1291b5b91fd493b35a742f
mugihouse <mugi.stnk100122@gmail.com>
2023-04-07 23:57:37 +0900

    Fix: login時入力内容を修正

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,9 @@
 module LoginMacros
   def login(user)
     visit admin_login_identifier_path
-    fill_in 'Username', with: 'admin'
+    fill_in 'Username', with: user.name
     click_button '次へ'
-    fill_in 'Password', with: 'password'
+    fill_in 'Password', with: user.password
     click_button 'ログイン'
   end
 end
