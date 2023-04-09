bf3b670031b969344ccd20247763965673571a67
mugihouse <mugi.stnk100122@gmail.com>
2023-04-10 08:28:34 +0900

    Add: testの変更

--- spec/system/article_block_previews_spec.rb
+++ spec/system/article_block_previews_spec.rb
@@ -21,4 +21,18 @@ RSpec.describe "ArticleBlockPreviews", type: :system do
       end
     end
   end
+
+  describe '記事作成画面でテキストブロックを追加'
+    let(:article) { create(:article) }
+    context 'テキストを追加せずプレビューを閲覧' do
+      it '正常に表示される' do
+        visit edit_admin_article_path(article.uuid)
+        click_link 'ブロックを追加する'
+        click_link 'プレビュー'
+        switch_to_window(windows.last)
+        expect(page).to have_content(article.title), 'プレビューページが正しく表示されていません'
+        expect(page).not_to have_content('no implicit conversion of nil into String'), 'プレビューページがエラー画面になっています'
+      end
+    end
+  end
 end
