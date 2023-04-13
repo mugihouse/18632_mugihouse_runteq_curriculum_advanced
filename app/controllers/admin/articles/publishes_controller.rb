e3916c468e08d9f3f1fae693de6d5aa98fcbb1dd
mugihouse <mugi.stnk100122@gmail.com>
2023-04-13 23:12:04 +0900

    Change: 更新・公開のアクションをstate, published_atによって挙動の変更

--- app/controllers/admin/articles/publishes_controller.rb
+++ app/controllers/admin/articles/publishes_controller.rb
@@ -5,7 +5,7 @@ class Admin::Articles::PublishesController < ApplicationController
 
   def update
     @article.published_at = Time.current unless @article.published_at?
-    @article.state = :published
+    @article.state = @article.publishable? ? :published : :publish_wait
 
     if @article.valid?
       Article.transaction do
@@ -13,13 +13,12 @@ class Admin::Articles::PublishesController < ApplicationController
         @article.save!
       end
 
-      flash[:notice] = '記事を公開しました'
-
+      flash[:notice] = @article.message_on_published
       redirect_to edit_admin_article_path(@article.uuid)
     else
       flash.now[:alert] = 'エラーがあります。確認してください。'
 
-      @article.state = @article.state_was if @article.state_changed?
+      @article.state = @article.state_was if @article.state_changed? # もしステータスが変わっていたら戻す
       render 'admin/articles/edit'
     end
   end
