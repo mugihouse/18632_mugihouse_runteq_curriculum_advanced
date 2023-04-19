90bf01e6cad096a5ad49b2d4129981c7cdf98e54
mugihouse <mugi.stnk100122@gmail.com>
2023-04-19 22:00:49 +0900

    Add: authorに関する検索アクション追加

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -3,12 +3,16 @@ class SearchArticlesForm
   include ActiveModel::Attributes
 
   attribute :category_id, :integer
+  attribute :author_id, :integer
   attribute :title, :string
 
   def search
+    # articleの重複レコードを一つにまとめる
     relation = Article.distinct
 
+    # category_idで検索
     relation = relation.by_category(category_id) if category_id.present?
+    relation = relation.by_author(author_id) if author_id.present?
     title_words.each do |word|
       relation = relation.title_contain(word)
     end
