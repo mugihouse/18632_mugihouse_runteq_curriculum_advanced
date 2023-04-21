2ce4504fc305b51e3daafd8bd4415b65cdb6df19
mugihouse <mugi.stnk100122@gmail.com>
2023-04-21 19:10:09 +0900

    Add: tag_idに関するscope追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -67,6 +67,7 @@ class Article < ApplicationRecord
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
   scope :by_category, ->(category_id) { where(category_id: category_id) }
   scope :by_author, ->(author_id) { where(author_id: author_id) }
+  scope :by_tag, ->(tag_id) { joins(:article_tags).where(article_tags: {tag_id: tag_id}) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
   scope :past_published, -> { where('published_at <= ?', Time.current) }
 
