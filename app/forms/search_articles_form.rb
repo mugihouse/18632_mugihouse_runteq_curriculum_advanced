cd852531551f6917d0784fdacf766d9cf05db466
mugihouse <mugi.stnk100122@gmail.com>
2023-04-23 13:02:50 +0900

    Add: searchアクションでbodyカラム検索の絞り込み

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -6,7 +6,7 @@ class SearchArticlesForm
   attribute :author_id, :integer
   attribute :tag_id, :integer
   attribute :title, :string
-  # attribute :body, :string
+  attribute :body, :string
 
   def search
     # articleの重複レコードを一つにまとめる
@@ -16,9 +16,9 @@ class SearchArticlesForm
     relation = relation.by_category(category_id) if category_id.present?
     relation = relation.by_author(author_id) if author_id.present?
     relation = relation.by_tag(tag_id) if tag_id.present?
-    # description_words.each do |word|
-    #   relation = relation.description_contain(word)
-    # end
+    body_words.each do |word|
+      relation = relation.body_contain(word)
+    end
     title_words.each do |word|
       relation = relation.title_contain(word)
     end
@@ -31,7 +31,7 @@ class SearchArticlesForm
     title.present? ? title.split(nil) : []
   end
 
-  # def description_words
-  #   description.present? ? description.split(nil) : []
-  # end
+  def body_words
+    body.present? ? body.split(nil) : []
+  end
 end
