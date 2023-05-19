b318d002a640c930146e5a2254eeb8cc83cbb9c4
mugihouse <mugi.stnk100122@gmail.com>
2023-05-19 16:33:16 +0900

    Change: mailerクラスにメール送信メソッドを作成

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,2 +1,7 @@
 class ArticleMailer < ApplicationMailer
+  def report_summary
+    @published_articles = params[:articles]
+    @articles_published_at_yesterday = @published_articles.where(published_at: Time.current.yesterday)
+    mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
+  end
 end
