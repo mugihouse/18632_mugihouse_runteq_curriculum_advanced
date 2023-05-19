3181d1ea63973348789cefbb0b9707f7066c5496
mugihouse <mugi.stnk100122@gmail.com>
2023-05-19 21:02:18 +0900

    Add: specファイル追加

--- spec/mailers/previews/article_mailer_preview.rb
+++ spec/mailers/previews/article_mailer_preview.rb
@@ -1,6 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/article_mailer
 class ArticleMailerPreview < ActionMailer::Preview
-  def activation_email
-    ArticleMailer.with(articles: Article.published).report_summary
-  end
+
 end
