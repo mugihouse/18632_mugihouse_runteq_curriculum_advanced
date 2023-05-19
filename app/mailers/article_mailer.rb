80c378076d826273aa840050deb6c13258d317a8
mugihouse <mugi.stnk100122@gmail.com>
2023-05-19 17:04:35 +0900

    Change: scopeメソッドを使用するよう変更

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,7 +1,7 @@
 class ArticleMailer < ApplicationMailer
   def report_summary
     @published_articles = params[:articles]
-    @articles_published_at_yesterday = @published_articles.where(published_at: Time.current.yesterday)
+    @articles_published_at_yesterday = @published_articles.published_at_yesterday
     mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
   end
 end
