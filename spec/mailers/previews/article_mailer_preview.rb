f8004866e10b3d1e503a6a7514e4a542b92428df
mugihouse <mugi.stnk100122@gmail.com>
2023-05-19 19:16:47 +0900

    Add: メール送信のプレビューテストを追加

--- spec/mailers/previews/article_mailer_preview.rb
+++ spec/mailers/previews/article_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/article_mailer
 class ArticleMailerPreview < ActionMailer::Preview
-
+  def activation_email
+    ArticleMailer.with(articles: Article.published).report_summary
+  end
 end
